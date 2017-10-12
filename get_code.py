24bf5862118ec4db389420442551098103cb4f3c
jmausolf <jgm346@gmail.com>
2017-10-12 15:39:42 -0500

    updates to get code file

--- get_code.py
+++ get_code.py
@@ -8,7 +8,7 @@ def extact_all_recipients():
     contacts = get_all_recipients()
     for c in contacts:
         r = str(c)
-        data = r.replace("ID", "").replace("Phone or email", "").replace(" ", "").replace("+", "").replace("tel:", "").replace("mailto:", "").split(":")
+        data = r.replace("ID", "").replace("Phone or email", "").replace(" ", "").replace("+", "").replace("tel:", "").replace("mailto:", "").replace("b'", "").split(":")
         cid = data[1]
         cnumber = data[2]
         results[cid] = cnumber
@@ -24,6 +24,8 @@ def get_ids_for_number(number):
 
 def return_amazon_code():
     ### Amazon Specific 2-Factor Codes
-    message = str(get_messages_for_recipient(int(get_ids_for_number("262966")))[0])
-    code = message.replace("Text: ", "").split(" ")[0]
+    message = str(get_messages_for_recipient(int(get_ids_for_number("262966")))[-1])
+    code = str(message.replace("Text: ", "").split(" ")[0]).replace("b'", "")
     return code
+
+print(return_amazon_code())
