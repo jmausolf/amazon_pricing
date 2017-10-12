eae45b9b1cb2eca178244db1a515746a43ad23b8
jmausolf <jgm346@gmail.com>
2017-10-12 01:26:39 -0500

    combining get_code into price_war. Need to resolve py2-py3 errors

--- price_war.py
+++ price_war.py
@@ -5,6 +5,9 @@ from selenium.webdriver.common.keys import Keys
 #Your Amazon Credentials File
 from credentials import *
 
+#2-Factor Code File
+from get_code import *
+
 
 def login():
     url = "https://sellercentral.amazon.com/inventory/ref=id_invmgr_dnav_xx_?tbla_myitable=sort:%7B%22sortOrder%22%3A%22DESCENDING%22%2C%22sortedColumnId%22%3A%22date%22%7D;search:;pagination:1;"
@@ -19,8 +22,12 @@ def login():
 
         password = driver.find_element_by_name("password")
         password.send_keys(amazon_password)
-        time.sleep(5)
+        time.sleep(15)
         password.send_keys(Keys.RETURN)
+
+        #TODO, make 2-factor code insert into web
+        time.sleep(7)
+        print(return_amazon_code())
 
     except:
         #Captcha Catch
